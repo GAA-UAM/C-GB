1f5468c6ea53167079880bab80bb901b25f7354d
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2021-10-10 23:14:05 +0200

    add the setup file'

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="cgb",
+    version="0.0.1",
+    author="Seyedsaman Emami, Gonzalo Martínez-Muñoz",
+    author_email="emami.seyedsaman@uam.es, gonzalo.martinez@uam.es",
+    description="Condensed Gradient Boosting Decision Tree",
+    packages=['cgb'],
+    install_requires=['numpy', 'numba', 'scikit-learn', 'scipy', 'numbers'],
+    classifiers=("Programming Language :: Python :: 3")
+)
